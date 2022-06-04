87179156cb291e8887090b1de5a15125e716c3eb
DanukaThimira <gamingvlab@gmail.com>
2022-06-04 10:20:48 -0700

    Netflix Clone

---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_scroll"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAHXPluq6GtTRPDIHRv5kJPy86uFjp5sO7hg&usqp=CAU"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
